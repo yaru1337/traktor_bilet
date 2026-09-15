6fe6d9480115ecde8a8c5d1be412741f636e527e
Dima <lencevichdmitriyy@gmail.com>
2026-09-15 18:48:18 +0300

    Update web build

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -10,7 +10,7 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final total = ticket.questions.length;
-    final passed = correct >= total - 1;
+    final passed = correct >= (total * 1).ceil();
 
     return Scaffold(
       appBar: AppBar(
